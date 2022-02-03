e0a5280f326e7b4d25f6ec100683bf0920e1dbf2
Hello World app <bakyt.ikramov@iaau.edu.kg>
2022-02-03 15:45:58 +0600

    Hello World App

---
+++ src/main/java/kg/edu/iaau/CS204Ikramov/Cs204IkramovApplication.java
@@ -0,0 +1,21 @@
+package kg.edu.iaau.CS204Ikramov;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Cs204IkramovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs204IkramovApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello, Bakyt!</h1>";
+	}
+}
---
+++ src/test/java/kg/edu/iaau/CS204Ikramov/Cs204IkramovApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.edu.iaau.CS204Ikramov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs204IkramovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
